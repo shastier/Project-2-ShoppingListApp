40ca5f32a52fb89aca9afe9d0b5aa7bb3440a81d
Shirley Hastier <shastier@yahoo.com>
2020-08-13 16:42:08 -0500

    Auth functionality added to items. Create, Update, Delete,

--- routes/item-router.js
+++ routes/item-router.js
@@ -2,19 +2,20 @@ const express = require('express');
 const itemRouter = express.Router();
 
 const itemsController = require('../controllers/items-controller');
+const authHelpers = require('../services/auth/auth-helpers');
 
 // root route, /items
 itemRouter.get('/', itemsController.index);
 
 // add new item route, /items/add
-itemRouter.post('/', itemsController.create);
+itemRouter.post('/', authHelpers.loginRequired, itemsController.create);
 
-itemRouter.get('/add', (req, res) => {
+itemRouter.get('/add', authHelpers.loginRequired, (req, res) => {
   res.render('items/add');
 });
 
 // delete an item route, /items/id
-itemRouter.delete('/:id', itemsController.delete);
+itemRouter.delete('/:id', authHelpers.loginRequired, itemsController.delete);
 
 // show details from an specific item, /items/:id
 itemRouter.get('/:id([0-9]+)', itemsController.show, (req, res) => {
@@ -24,13 +25,13 @@ itemRouter.get('/:id([0-9]+)', itemsController.show, (req, res) => {
 });
 
 // edit an item route, /items/:id/edit
-itemRouter.get('/:id([0-9]+)/edit', itemsController.show, (req, res) => {
+itemRouter.get('/:id([0-9]+)/edit', authHelpers.loginRequired, itemsController.show, (req, res) => {
     res.render('items/edit', {
         item: res.locals.item,
     });
 });
 
 // update an item route, /items/:id
-itemRouter.put('/:id', itemsController.update);
+itemRouter.put('/:id', authHelpers.loginRequired, itemsController.update);
 
 module.exports = itemRouter;
