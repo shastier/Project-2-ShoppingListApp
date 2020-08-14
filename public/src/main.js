f078501cece738ffa053b60a745a4e997b0445e5
Shirley Hastier <shastier@yahoo.com>
2020-08-14 12:31:03 -0500

    DOM manipulation, index.ejs page

--- public/src/main.js
+++ public/src/main.js
@@ -1,1 +1,17 @@
 console.log('main.js is connected!');
+
+function mouseOver(e){
+    e.target.classList.add('colored_in');    
+}
+function mouseOut(e){
+    e.target.classList.remove('colored_in');    
+}
+
+window.onload = function(){
+    document.querySelector('#about').addEventListener('mouseover', mouseOver);
+    document.querySelector('#about').addEventListener('mouseout', mouseOut);
+    document.querySelector('#all').addEventListener('mouseover', mouseOver);
+    document.querySelector('#all').addEventListener('mouseout', mouseOut);
+    document.querySelector('#home').addEventListener('mouseover', mouseOver);
+    document.querySelector('#home').addEventListener('mouseout', mouseOut);
+};
